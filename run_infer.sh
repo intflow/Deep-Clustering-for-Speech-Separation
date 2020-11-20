68ffa90db0e9be6d7cbd49ba73df3d4e8f3eb710
kmjeon <lordet01@gmail.com>
2020-11-20 08:21:45 +0000

    v3 WIP

--- run_infer.sh
+++ run_infer.sh
@@ -2,9 +2,5 @@
 
 
 python create_scp_test.py
-<<<<<<< HEAD
-python test.py -scp ./scp/tt_mix.scp -opt ./config/train.yml -save_file /DL_data_big/AIGC_3rd_track3/2020_track3_rev/t3_audio.TAUVAD.DPCL
-=======
 python test.py -scp ./scp/tt_mix.scp -opt ./config/train.yml -save_file ../wav_tmp
->>>>>>> a37b103c6e5a6ab8dc44b101d8d8a0ef5b397ff4
 #python test.py -scp ./scp/tt_mix.scp -opt ./config/train.yml -save_file /DL_data_big/AIGC_3rd_track3/Mono_100_test/DPCL
